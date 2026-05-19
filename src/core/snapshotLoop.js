968bee214cd63f54d3c215fcae4ae8e3966335db
luizgrs97 <luizgrs97@gmail.com>
2026-05-19 20:05:30 -0300

    Correção de carregamento em redes lentas

--- src/core/snapshotLoop.js
+++ src/core/snapshotLoop.js
@@ -22,12 +22,27 @@ function sendSnapshot(io, players, territories) {
             socket.data.snapshotState = createClientSnapshotState();
         }
 
-        socket.volatile.emit(
-            "gameState",
-            createSnapshot(players, territories, socket.id, socket.data.snapshotState)
-        );
+        const snapshot = createSnapshot(players, territories, socket.id, socket.data.snapshotState);
+        const emitter = shouldSendReliably(snapshot) ? socket : socket.volatile;
+
+        emitter.emit("gameState", snapshot);
     }
+}
+
+function shouldSendReliably(snapshot) {
+    return hasEntries(snapshot.playerInfo)
+        || hasEntries(snapshot.territories)
+        || hasFullTrailUpdate(snapshot.trails);
+}
+
+function hasEntries(value) {
+    return value && Object.keys(value).length > 0;
+}
+
+function hasFullTrailUpdate(trails) {
+    return Object.values(trails || {}).some(trail => trail && trail.full);
 }
 
 module.exports = startSnapshotLoop;
 module.exports.sendSnapshot = sendSnapshot;
+module.exports.shouldSendReliably = shouldSendReliably;
